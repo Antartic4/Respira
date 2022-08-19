740229350e004daf965db787cad9c0a230ba68e5
Antartic4 <robjose1996@hotmail.com>
2022-08-19 16:30:48 -0400

    6to commit fix login/signup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchema, ILogin } from "../common/validation/auth";
 import Layout from "../common/components/Layout";
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
   const { register, handleSubmit } = useForm<ILogin>({
     resolver: zodResolver(loginSchema),
   });
@@ -27,36 +27,72 @@ const Home: NextPage = () => {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <main className="bg-purple-200">
-          <form
-            className="flex items-center justify-center w-full h-screen"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div className="bg-purple-300 shadow-xl card w-96">
-              <div className="card-body">
-                <h2 className="card-title">¡Bienvenidos!</h2>
-                <input
-                  type="email"
-                  placeholder="Correo electronico"
-                  className="w-full max-w-xs mt-2 input input-bordered"
-                  {...register("email")}
-                />
-                <input
-                  type="password"
-                  placeholder="Contraseña"
-                  className="w-full max-w-xs my-2 input input-bordered"
-                  {...register("password")}
-                />
-                <div className="items-center justify-between card-actions">
-                  <Link href="/sign-up" className="link">
-                    Crear una cuenta
-                  </Link>
-                  <button className="btn btn-base" type="submit">
-                    Iniciar Session
-                  </button>
+          {/* Div Mobile Reactive */}
+          <div className="xl:hidden">
+            <form
+              className="flex items-center justify-center w-full h-screen"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div className="bg-purple-300 m:hidden shadow-xl card w-96">
+                <div className="card-body">
+                  <h2 className="card-title">¡Bienvenidos!</h2>
+                  <input
+                    type="email"
+                    placeholder="Correo electronico"
+                    className="w-full max-w-xs mt-2 input input-bordered"
+                    {...register("email")}
+                  />
+                  <input
+                    type="password"
+                    placeholder="Contraseña"
+                    className="w-full max-w-xs my-2 input input-bordered"
+                    {...register("password")}
+                  />
+                  <div className="items-center justify-between card-actions">
+                    <Link href="/sign-up" className="link">
+                      Crear una cuenta
+                    </Link>
+                    <button className="btn btn-base" type="submit">
+                      Iniciar Session
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
+          {/* Desktop */}
+          <div className="invisible xl:visible">
+            <form
+              className="flex items-center justify-center w-full h-screen"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div className="bg-purple-300 shadow-xl card w-96">
+                <div className="card-body">
+                  <h2 className="card-title">¡Bienvenidos!</h2>
+                  <input
+                    type="email"
+                    placeholder="Correo electronico"
+                    className="w-full max-w-xs mt-2 input input-bordered"
+                    {...register("email")}
+                  />
+                  <input
+                    type="password"
+                    placeholder="Contraseña"
+                    className="w-full max-w-xs my-2 input input-bordered"
+                    {...register("password")}
+                  />
+                  <div className="items-center justify-between card-actions">
+                    <Link href="/sign-up" className="link">
+                      Crear una cuenta
+                    </Link>
+                    <button className="btn btn-base" type="submit">
+                      Iniciar Session
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
         </main>
       </div>
     </Layout>
